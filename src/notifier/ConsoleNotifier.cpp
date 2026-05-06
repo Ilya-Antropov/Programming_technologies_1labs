ce5b74640cd046ee386118e2a493eb7e4bd5d4a8
Ilya-Antropov <ilya.antropov2018@mail.ru>
2026-05-06 15:33:55 +0500

    Реализовал форматирование сообщений в ConsoleNotifier и добавил CMakeLists.txt
    В ConsoleNotifier.cpp реализован метод formatMessage(), который формирует читаемое описание события CheckEvent: FILE_EXISTS, FILE_CHANGED, FILE_DELETED, FILE_RESTORED. Так же добавлен CMakeLists для сборки проекта

--- src/notifier/ConsoleNotifier.cpp
+++ src/notifier/ConsoleNotifier.cpp
@@ -10,4 +10,26 @@ void ConsoleNotifier::notify(const CheckResult& result) {
     out.flush();
 }
 
-// тут будет formatMessage
+QString ConsoleNotifier::formatMessage(const CheckResult& result) {
+    switch (result.event) {
+        case CheckEvent::NONE:
+            return {};
+
+        case CheckEvent::FILE_EXISTS:
+            return QStringLiteral("  [+] Файл существует: '%1'  (%2 байт)")
+                .arg(result.path).arg(result.size);
+
+        case CheckEvent::FILE_CHANGED:
+            return QStringLiteral("  [~] Файл изменён: '%1'  %2 -> %3 байт")
+                .arg(result.path).arg(result.oldSize).arg(result.size);
+
+        case CheckEvent::FILE_DELETED:
+            return QStringLiteral("  [-] Файл удалён: '%1'")
+                .arg(result.path);
+
+        case CheckEvent::FILE_RESTORED:
+            return QStringLiteral("  [*] Файл восстановлен: '%1'  (%2 байт)")
+                .arg(result.path).arg(result.size);
+    }
+    return {};
+}
